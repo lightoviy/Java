6b18831b75db6cb6cc6fb72c7dcc73c5508cd41b
lightoviy <145274081+lightoviy@users.noreply.github.com>
2024-12-12 01:08:11 +0100

    Add files via upload

--- suda_zewnetrzny.js
+++ suda_zewnetrzny.js
@@ -1,2 +1,2 @@
-document.write("Ostatnia modyfikacja strony".fontcolor("#FF0000").bold().fontsize(6)+"<br>"); 
-document.write(document.lastModified.fontcolor("#FF0000").bold()); 
+document.write("Ostatnia modyfikacja strony".fontcolor("#C0C0C0").bold().fontsize(6)+"<br>"); 
+document.write(document.lastModified.fontcolor("#C0C0C0").bold()); 
